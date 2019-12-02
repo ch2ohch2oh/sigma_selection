5c01d6b60b201e2d089dcfc4b71f7a133e59df6e
ch2ohch2oh <ch2ohch2oh@gmail.com>
2019-12-03 08:50:50 +0900

    All new job submission script copied from elsewhere

--- scripts/submit_jobs.py
+++ scripts/submit_jobs.py
@@ -10,61 +10,79 @@ import sys
 import argparse
 import logging
 import shutil
+from termcolor import colored
+from multiprocessing import Pool
+import tqdm
 
-parser = argparse.ArgumentParser()
-parser.add_argument('script', help = 'Script to run')
-parser.add_argument('--mc', help = 'Is MC or not')
-parser.add_argument('--test', action = 'store_true', default = False,
-                    help = 'Process the first mdst for testing')
-parser.add_argument('--clear', action = 'store_true', default = False,
-                   help = 'Clear output dir or not')
-parser.add_argument('--output', help = 'Output dir')
+def get_dataset(name):
+    """Retrieve list of mdst files based on the name of the dataset"""
+    if name == 'mc_exp55':
+        return b2c.parse_process_url('http://bweb3.cc.kek.jp/montecarlo.php?ex=55&rs=1&re=50&ty=Any&dt=Any&bl=caseB&st=0')
+    elif name == 'mc_background':
+        return b2c.parse_process_url('http://bweb3.cc.kek.jp/montecarlo.php?ex=55&rs=1&re=30&ty=Any&dt=Any&bl=caseB&st=0')
+    elif name == 'mc_exp65':
+        return b2c.parse_process_url('http://bweb3.cc.kek.jp/montecarlo.php?ex=65&rs=1&re=25&ty=Any&dt=Any&bl=caseB&st=0')
+    elif name == 'exp65_4S':
+        return b2c.parse_process_url('http://bweb3.cc.kek.jp/mdst.php?ex=65&rs=1&re=100&skm=HadronBorJ&dt=on_resonance&bl=caseB')
+    elif name == 'exp65_1S':
+        return b2c.parse_process_url('http://bweb3.cc.kek.jp/mdst.php?ex=65&rs=1000&re=1050&skm=HadronBorJ&dt=1S_scan&bl=caseB')
+    elif name == 'exp65_cont':
+        return b2c.parse_process_url('http://bweb3.cc.kek.jp/mdst.php?ex=65&rs=0&re=9999&skm=HadronBorJ&dt=continuum&bl=caseB')
+    else:
+        raise Exception(f'Dataset ({name}) not found')
 
-args = parser.parse_args()
+def submit_one(script, mdstpath, outdir, b2opt=""):
+    """Submit one job for one mdst file"""
+#     print(mdstpath)
+    base = os.path.basename(mdstpath)
+    rootname = base + '.root'
+    logname = base + '.log'
+    rootpath = os.path.join(outdir, rootname)
+    logpath = os.path.join(outdir, logname)
+    os.system(f'bsub -q s -oo {logpath} basf2 {b2opt} {script} {mdstpath} {rootpath} >> /dev/null')
+    
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('script', help = 'Script to run')
+    parser.add_argument('data', help = 'Which data set to use?')
+    parser.add_argument('--one', action = 'store_true', default = False,
+                        help = 'Process the first mdst for testing')
+    parser.add_argument('--clear', action = 'store_true', default = False,
+                       help = 'Clear output dir or not')
+    parser.add_argument('outdir', help = 'Output dir')
 
-list_mdst = []
-output_dir = args.output
-script = args.script
+    args = parser.parse_args()
+    
+    # Give user some information about the script and data set
+    print(f'Script to run: {args.script}')
+    print(f'Dataset: {args.data}')
+    dataset = get_dataset(args.data)
+    assert len(dataset) > 0, "Empty dataset!"
+    print('The dataset contains %d mdst files' % len(dataset))
+    if args.one == True:
+        print(colored('Test mode on: will only run on the first mdst file', 'red'))
+        dataset = dataset[:1]
+    
+    outdir = args.outdir
+    print(f'Output dir: {outdir}')
+    
+    # Create output dir if not existing
+    os.makedirs(outdir, exist_ok = True)
+    if not os.path.exists(outdir):
+        raise Exception("Failed to create output dir")
+    
+    # Clear the output dir if asked
+    if args.clear == True:
+        print(colored('Clearing output directory...', 'red'))
+        shutil.rmtree(outdir)
+        os.mkdir(outdir)
+        assert os.path.exists(outdir), "Output directory does not exist!"
+        print(colored('Done', 'green'))
 
-if script == 'pi0_mc.py':
-    # Do not need too much data for pi0
-    list_mdst = b2c.parse_process_url('http://bweb3.cc.kek.jp/montecarlo.php?ex=55&rs=1&re=20&ty=Any&dt=on_resonance&bl=caseB&st=1')
-    assert(len(list_mdst) == 16)
-elif script == 'sigma_mc.py':
-    # Need much more data for simga
-    list_mdst = b2c.parse_process_url('http://bweb3.cc.kek.jp/montecarlo.php?ex=55&rs=1&re=30&ty=Any&dt=on_resonance&bl=caseB&st=1')
-    assert(len(list_mdst) == 40)
-else:
-    print("Script not recognized!")
-    sys.exit(-1)
-
-if args.test == True:
-    list_mdst = list_mdst[:1]
-    print("Test: only keep the first mdst file to process")
-
-print('Script: %s' % script)
-print('Output dir: %s' % output_dir)
-print('Number of mdst files: %d' % len(list_mdst))
-
-os.makedirs(output_dir, exist_ok = True)
-if not os.path.exists(output_dir):
-    print("Output dir does not exist!")
-    sys.exit(-1)
-
-if args.clear == True:
-    print("Clear output directory...")
-    shutil.rmtree(output_dir)
-    os.mkdir(output_dir)
-
-
-print('Submit jobs'.center(60, '='))
-
-for mdst_id, mdst_path in enumerate(list_mdst, 1):
-    mdst_name = os.path.basename(mdst_path)
-    root_name = mdst_name + '.root'
-    log_name = mdst_name + '.log'
-    output_path = os.path.join(output_dir, root_name)
-    log_path =  os.path.join(output_dir, log_name)
-    print("[%d/%d]" % (mdst_id, len(list_mdst)))
-    os.system('bsub -q s -oo {log_path} basf2 {script} {mdst_path} {output_path}'.format(**locals()))
-
+    bar = tqdm.tqdm(total = len(dataset))
+    b2opt = ""
+    if args.one == True:
+        b2opt = "-n 1000"
+    for mdst in dataset:
+        submit_one(args.script, mdst, args.outdir, b2opt)
+        bar.update()
